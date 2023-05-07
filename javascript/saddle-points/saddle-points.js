fb1fb748811c35f00a12fb2b81b3fd2ef5f3d1ea
Víctor Carmona <vicarali@gmail.com>
2023-05-07 15:25:32 +0200

    Submit solution of saddle-points exercise

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,31 +1,64 @@
 export const saddlePoints = (treeMatrix) => {
   let saddlePointsCoordinates = [];
   
-  treeMatrix.forEach(treesRow => {
+  if(treeMatrix.length > 1) {
+    treeMatrix.forEach((treesRow, i) => {
+      let biggestTreeOnRow = Math.max(...treesRow);
+      let biggestTreeOnRowIndexes = [];
+
+      treesRow.forEach((tree, n) => {
+        if(tree == biggestTreeOnRow)
+          biggestTreeOnRowIndexes.push(n);
+      });
+
+      biggestTreeOnRowIndexes.forEach(
+        checkBiggestTreeOnColumn(
+          treeMatrix, 
+          biggestTreeOnRow, 
+          saddlePointsCoordinates, 
+          i
+        )
+      );
+    });
+  }
+  else {
+    let treesRow = treeMatrix[0];
     let biggestTreeOnRow = Math.max(...treesRow);
-    let biggestTreeOnRowIndex = treesRow.indexOf(biggestTreeOnRow);
+    let biggestTreeOnRowIndexes = [];
+
+    treesRow.forEach((tree, n) => {
+      if(tree == biggestTreeOnRow)
+        biggestTreeOnRowIndexes.push(n);
+    });
+
+    biggestTreeOnRowIndexes.forEach(treeIndex => {
+      saddlePointsCoordinates.push({
+        "row": 1, 
+        "column": treeIndex + 1
+      });
+    });
+  }
+  
+  return saddlePointsCoordinates;
+};
+
+function checkBiggestTreeOnColumn(treeMatrix, biggestTreeOnRow, saddlePointsCoordinates, i) {
+  return treeIndex => {
     let smallestTreeOnColumn = true;
 
-    for(let i = 0; i < treeMatrix.length; i++) {
-      if(treeMatrix[i][biggestTreeOnRowIndex] < biggestTreeOnRow) {
+    for (let n = 0; n < treeMatrix.length; n++) {
+      if (treeMatrix[n][treeIndex] < biggestTreeOnRow) {
         smallestTreeOnColumn = false;
         break;
       }
     }
 
-    if(smallestTreeOnColumn) {
+    if (smallestTreeOnColumn) {
       saddlePointsCoordinates.push({
-        "row": treeMatrix.indexOf(treesRow) + 1, 
-        "column": treesRow.indexOf(biggestTreeOnRow) + 1
+        "row": i + 1,
+        "column": treeIndex + 1
       });
     }
-  });
+  };
+}
 
-  return saddlePointsCoordinates;
-};
-
-saddlePoints([
-  [9, 8, 7],
-  [5, 3, 2],
-  [6, 6, 7],
-]);
