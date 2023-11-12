e51ccb8218b542731d328e5cce39eb45d811d40e
VictorC <vicarali@gmail.com>
2023-11-12 16:02:14 +0100

    Java: Solution for resistor-color exercise

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,10 +1,19 @@
 import java.util.*;
 
 class ResistorColor {
-    Map<String, Integer> colorsDictionary = new HashMap<String, Integer>();
+    Map<String, Integer> colorsDictionary = new LinkedHashMap<String, Integer>();
 
     public ResistorColor() {
         colorsDictionary.put("black", 0);
+        colorsDictionary.put("brown", 1);
+        colorsDictionary.put("red", 2);
+        colorsDictionary.put("orange", 3);
+        colorsDictionary.put("yellow", 4);
+        colorsDictionary.put("green", 5);
+        colorsDictionary.put("blue", 6);
+        colorsDictionary.put("violet", 7);
+        colorsDictionary.put("grey", 8);
+        colorsDictionary.put("white", 9);
     }
 
     int colorCode(String color) {
@@ -12,6 +21,7 @@ class ResistorColor {
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Set<String> colorKeys = colorsDictionary.keySet();
+        return colorKeys.toArray(new String[colorKeys.size()]);
     }
 }
--- java/resistor-color/src/test/java/ResistorColorTest.java
+++ java/resistor-color/src/test/java/ResistorColorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResistorColorTest {
@@ -17,19 +18,16 @@ public class ResistorColorTest {
         assertThat(resistorColor.colorCode("black")).isEqualTo(0);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testWhiteColorCode() {
         assertThat(resistorColor.colorCode("white")).isEqualTo(9);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testOrangeColorCode() {
         assertThat(resistorColor.colorCode("orange")).isEqualTo(3);
     }
-
-    @Ignore("Remove to run test")
+    
     @Test
     public void testColors() {
         assertThat(resistorColor.colors()).containsExactly(
