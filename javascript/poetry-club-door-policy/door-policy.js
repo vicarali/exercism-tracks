ceaa7899c56633d491311721bfd30da63d078899
VictorC <vicarali@gmail.com>
2024-10-08 17:26:11 +0200

    Solution for the poetry club door policy exercise

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+	return line[0];
 }
 
 /**
@@ -38,7 +38,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+	return capitalizeWord(word);
 }
 
 /**
@@ -49,7 +49,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+	const trimmedLine = line.trim();
+	return trimmedLine[trimmedLine.length - 1];
 }
 
 /**
@@ -60,5 +61,12 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+	const capitalizedWord = capitalizeWord(word);
+	return `${capitalizedWord}, please`;
+}
+
+function capitalizeWord(word) {
+	const lowerCaseWord = word.toLowerCase();
+	const capitalizedLetter = word[0].toUpperCase();
+	return `${capitalizedLetter + lowerCaseWord.slice(1)}`;
 }
