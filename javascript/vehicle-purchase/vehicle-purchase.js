b26ffc1d482ab366ef807674c1579505df4d5905
Vicarali <vicarali@gmail.com>
2024-10-26 19:59:06 +0200

    Solution for vehicle-purchase exercise

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+	return kind === "car" || kind === "truck";
 }
 
 /**
@@ -23,7 +23,12 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+	let chosenVehicle;
+
+	if (option1 < option2) chosenVehicle = option1;
+	else chosenVehicle = option2;
+
+	return `${chosenVehicle} is clearly the better choice.`;
 }
 
 /**
@@ -35,5 +40,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+	let estimatedPrice;
+
+	if (age < 3) estimatedPrice = originalPrice * 0.8;
+	else if (age > 10) estimatedPrice = originalPrice * 0.5;
+	else estimatedPrice = originalPrice * 0.7;
+
+	return estimatedPrice;
 }
