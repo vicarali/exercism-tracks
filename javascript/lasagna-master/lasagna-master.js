ca0a1fdc41e2c7add4bdfc7427d48506ca8e02bf
Victor Carmona <Victor.Carmona.extern@ista.com>
2024-11-20 17:45:34 +0100

    feat: solution for lasagna-master exercise

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,12 +1,57 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
-/**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
- */
+export function cookingStatus(remainingTime) {
+	switch (remainingTime) {
+		case 0:
+			return "Lasagna is done.";
+		case undefined:
+			return "You forgot to set the timer.";
+		default:
+			return "Not done, please wait.";
+	}
+}
+
+export function preparationTime(layers, timePerLayer = 2) {
+	return layers.length * timePerLayer;
+}
+
+export function quantities(layers) {
+	let noodles = 0;
+	let sauce = 0;
+
+	layers.forEach((layer) => {
+		if (layer === "noodles") noodles += 50;
+		else if (layer === "sauce") sauce += 0.2;
+	});
+
+	return {
+		noodles: noodles,
+		sauce: sauce
+	};
+}
+
+export function addSecretIngredient(friendsList, myList) {
+	myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipe, portions) {
+	const recipeForOne = calculateRecipeForOne(recipe);
+
+	for (const key in recipeForOne) {
+		recipeForOne[key] *= portions;
+	}
+
+	return recipeForOne;
+}
+
+function calculateRecipeForOne(recipe) {
+	let recipeForOne = {};
+
+	for (const key in recipe) {
+		recipeForOne[key] = recipe[key] / 2;
+	}
+
+	return recipeForOne;
+}
+
