d062f737f21de0023e10cc0ccd1ea7828872c5c8
Victor Carmona <Victor.Carmona.extern@ista.com>
2024-12-19 17:45:06 +0100

    feat: solution for factory-sensors exercise

--- javascript/factory-sensors/factory-sensors.js
+++ javascript/factory-sensors/factory-sensors.js
@@ -16,7 +16,8 @@ export class OverheatingError extends Error {
  * @throws {Error}
  */
 export function checkHumidityLevel(humidityPercentage) {
-  throw new Error('Implement the checkHumidity function');
+	if (humidityPercentage > 70)
+		throw new Error(`Humidity percentage too high (${humidityPercentage})`);
 }
 
 /**
@@ -26,7 +27,8 @@ export function checkHumidityLevel(humidityPercentage) {
  * @throws {ArgumentError|OverheatingError}
  */
 export function reportOverheating(temperature) {
-  throw new Error('Implement the reportOverheating function');
+	if (temperature === null) throw new ArgumentError();
+	if (temperature > 500) throw new OverheatingError(temperature);
 }
 
 /**
@@ -41,5 +43,17 @@ export function reportOverheating(temperature) {
  * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
-  throw new Error('Implement the monitorTheMachine function');
+  try {
+		const currentTemperature = actions.check();
+		reportOverheating(currentTemperature);
+	} catch (error) {
+		if (error instanceof ArgumentError) {
+			actions.alertDeadSensor();
+		} else if (error instanceof OverheatingError) {
+			if (error.temperature < 600) actions.alertOverheating();
+			else actions.shutdown();
+		} else {
+			throw error;
+		}
+	}
 }
