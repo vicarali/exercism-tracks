9b3df31f6f409692842d41fe522ddff488ccdabe
Victor Carmona <Victor.Carmona.extern@ista.com>
2025-02-08 10:41:59 +0100

    feat: first iteration of translation-service exercise

--- javascript/translation-service/service.js
+++ javascript/translation-service/service.js
@@ -9,67 +9,75 @@
 // In your own projects, files, and code, you can play with @ts-check as well.
 
 export class TranslationService {
-  /**
-   * Creates a new service
-   * @param {ExternalApi} api the original api
-   */
-  constructor(api) {
-    this.api = api;
-  }
+	/**
+	 * Creates a new service
+	 * @param {ExternalApi} api the original api
+	 */
+	constructor(api) {
+		this.api = api;
+	}
 
-  /**
-   * Attempts to retrieve the translation for the given text.
-   *
-   * - Returns whichever translation can be retrieved, regardless the quality
-   * - Forwards any error from the translation api
-   *
-   * @param {string} text
-   * @returns {Promise<string>}
-   */
-  free(text) {
-    throw new Error('Implement the free function');
-  }
+	/**
+	 * Attempts to retrieve the translation for the given text.
+	 *
+	 * - Returns whichever translation can be retrieved, regardless the quality
+	 * - Forwards any error from the translation api
+	 *
+	 * @param {string} text
+	 * @returns {Promise<string>}
+	 */
+	async free(text) {
+		return this.api
+			.fetch(text)
+			.then((promise) => {
+				return promise.translation;
+			})
+			.catch((error) => {
+				throw error;
+			});
+	}
 
-  /**
-   * Batch translates the given texts using the free service.
-   *
-   * - Resolves all the translations (in the same order), if they all succeed
-   * - Rejects with the first error that is encountered
-   * - Rejects with a BatchIsEmpty error if no texts are given
-   *
-   * @param {string[]} texts
-   * @returns {Promise<string[]>}
-   */
-  batch(texts) {
-    throw new Error('Implement the batch function');
-  }
+	/**
+	 * Batch translates the given texts using the free service.
+	 *
+	 * - Resolves all the translations (in the same order), if they all succeed
+	 * - Rejects with the first error that is encountered
+	 * - Rejects with a BatchIsEmpty error if no texts are given
+	 *
+	 * @param {string[]} texts
+	 * @returns {Promise<string[]>}
+	 */
+	batch(texts) {
+    const translations
+		return Promise.all();
+	}
 
-  /**
-   * Requests the service for some text to be translated.
-   *
-   * Note: the request service is flaky, and it may take up to three times for
-   *       it to accept the request.
-   *
-   * @param {string} text
-   * @returns {Promise<void>}
-   */
-  request(text) {
-    throw new Error('Implement the request function');
-  }
+	/**
+	 * Requests the service for some text to be translated.
+	 *
+	 * Note: the request service is flaky, and it may take up to three times for
+	 *       it to accept the request.
+	 *
+	 * @param {string} text
+	 * @returns {Promise<void>}
+	 */
+	request(text) {
+		throw new Error("Implement the request function");
+	}
 
-  /**
-   * Retrieves the translation for the given text
-   *
-   * - Rejects with an error if the quality can not be met
-   * - Requests a translation if the translation is not available, then retries
-   *
-   * @param {string} text
-   * @param {number} minimumQuality
-   * @returns {Promise<string>}
-   */
-  premium(text, minimumQuality) {
-    throw new Error('Implement the premium function');
-  }
+	/**
+	 * Retrieves the translation for the given text
+	 *
+	 * - Rejects with an error if the quality can not be met
+	 * - Requests a translation if the translation is not available, then retries
+	 *
+	 * @param {string} text
+	 * @param {number} minimumQuality
+	 * @returns {Promise<string>}
+	 */
+	premium(text, minimumQuality) {
+		throw new Error("Implement the premium function");
+	}
 }
 
 /**
