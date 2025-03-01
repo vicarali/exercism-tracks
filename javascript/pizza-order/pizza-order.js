6127ceefd562b39b720572eafc6d284b13b395f1
Victor Carmona <Victor.Carmona.extern@ista.com>
2025-03-01 12:49:00 +0100

    chore: set up pizza-order exercise

---
+++ javascript/pizza-order/pizza-order.js
@@ -0,0 +1,28 @@
+/// <reference path="./global.d.ts" />
+//
+// @ts-check
+
+/**
+ * Determine the price of the pizza given the pizza and optional extras
+ *
+ * @param {Pizza} pizza name of the pizza to be made
+ * @param {Extra[]} extras list of extras
+ *
+ * @returns {number} the price of the pizza
+ */
+export function pizzaPrice(pizza, ...extras) {
+  throw new Error('Please implement the pizzaPrice function');
+}
+
+/**
+ * Calculate the price of the total order, given individual orders
+ *
+ * (HINT: For this exercise, you can take a look at the supplied "global.d.ts" file
+ * for a more info about the type definitions used)
+ *
+ * @param {PizzaOrder[]} pizzaOrders a list of pizza orders
+ * @returns {number} the price of the total order
+ */
+export function orderPrice(pizzaOrders) {
+  throw new Error('Please implement the orderPrice function');
+}
