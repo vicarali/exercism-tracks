5a3b08e1405ec6b417f187cd71fa36f2b0c18471
Victor Carmona <Victor.Carmona.extern@ista.com>
2025-03-12 18:12:06 +0100

    feat: first iteration squeaky-clean exercise

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,33 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        String identifierWithoutWhitespaces = replaceWhitespaces(identifier);
+
+        return identifierWithoutWhitespaces;
+    }
+
+    static String replaceWhitespaces(String identifier) {
+        StringBuilder sb = new StringBuilder(identifier);
+
+        int currentWhiteSpaceOccurence = sb.indexOf(" ");
+        while(currentWhiteSpaceOccurence != -1) {
+            sb.replace(currentWhiteSpaceOccurence, currentWhiteSpaceOccurence+1, "_");
+            currentWhiteSpaceOccurence = sb.indexOf(" ");
+        }
+
+        return sb.toString();
+    }
+
+    static String convertFromKebabCaseToCamelCase(String identifier) {
+        StringBuilder sb = new StringBuilder(identifier);
+
+        int currentHyphenOccurence = sb.indexOf("-");
+        while(currentHyphenOccurence != -1) {
+            sb.deleteCharAt(currentHyphenOccurence);
+
+            char nextChar = sb.charAt(currentHyphenOccurence);
+            sb.replaceCharAt(nextChar)
+        }
+
+        return sb.toString();
     }
 }
