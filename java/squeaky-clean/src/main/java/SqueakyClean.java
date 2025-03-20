f76549fb4d6eadc42443b2eb5821a594f6e476d2
vca <vca@ciklum.com>
2025-03-20 13:53:14 +0100

    feat(java): solution for squeaky-clean exercise

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,11 +1,16 @@
+import java.util.HashMap;
+
 class SqueakyClean {
     static String clean(String identifier) {
         String identifierWithoutWhitespaces = replaceWhitespaces(identifier);
+        String identifierInCamelCase = convertFromKebabCaseToCamelCase(identifierWithoutWhitespaces);
+        String identifierWithoutLeetSpeak = convertFromLeetSpeakToNormalText(identifierInCamelCase);
+        String identifierWithoutNonLetters = removeNonLetters(identifierWithoutLeetSpeak);
 
-        return identifierWithoutWhitespaces;
+        return identifierWithoutNonLetters;
     }
 
-    static String replaceWhitespaces(String identifier) {
+    private static String replaceWhitespaces(String identifier) {
         StringBuilder sb = new StringBuilder(identifier);
 
         int currentWhiteSpaceOccurence = sb.indexOf(" ");
@@ -17,7 +22,7 @@ class SqueakyClean {
         return sb.toString();
     }
 
-    static String convertFromKebabCaseToCamelCase(String identifier) {
+    private static String convertFromKebabCaseToCamelCase(String identifier) {
         StringBuilder sb = new StringBuilder(identifier);
 
         int currentHyphenOccurence = sb.indexOf("-");
@@ -25,7 +30,46 @@ class SqueakyClean {
             sb.deleteCharAt(currentHyphenOccurence);
 
             char nextChar = sb.charAt(currentHyphenOccurence);
-            sb.replaceCharAt(nextChar)
+            String capitalizedNextCharString = String.valueOf(nextChar).toUpperCase();
+            sb.replace(currentHyphenOccurence, currentHyphenOccurence+1, capitalizedNextCharString);
+
+            currentHyphenOccurence = sb.indexOf("-");
+        }
+
+        return sb.toString();
+    }
+
+    private static String convertFromLeetSpeakToNormalText(String identifier) {
+        StringBuilder sb = new StringBuilder(identifier);
+
+        HashMap<Character, Character> leetSpeakCharacters = new HashMap<>();
+        leetSpeakCharacters.put('4', 'a');
+        leetSpeakCharacters.put('3', 'e');
+        leetSpeakCharacters.put('0', 'o');
+        leetSpeakCharacters.put('1', 'l');
+        leetSpeakCharacters.put('7', 't');
+
+        for (int i = 0; i < sb.length(); i++) {
+            char currentChar = sb.charAt(i);
+
+            if(leetSpeakCharacters.containsKey(currentChar)) {
+                sb.replace(i, i+1, leetSpeakCharacters.get(currentChar).toString());
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String removeNonLetters(String identifier) {
+        StringBuilder sb = new StringBuilder(identifier);
+
+        for (int i = 0; i < sb.length(); i++) {
+            char currentChar = sb.charAt(i);
+
+            if(!Character.isLetter(currentChar) && currentChar != '_') {
+                sb.deleteCharAt(i);
+                i--;
+            }
         }
 
         return sb.toString();
